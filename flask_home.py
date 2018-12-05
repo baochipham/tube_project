2278e2ab274aaa6add5632fe54e6c9e72726acf7
baochipham <baochipham08@gmail.com>
2018-12-05 18:16:41 +0000

    added about and show sections

--- flask_home.py
+++ flask_home.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
-from trial import get_tweets
+from trial_3 import delay_tweets
 app = Flask(__name__)
 
 
 @app.route('/') #routing
 def home():
-    tweets = get_tweets()
+    tweets = delay_tweets()
     return render_template('tweet_embedder.html', tweets=tweets)
+    #return 'work in progress'
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/show/<tube_line>')
+def show(tube_line):
+    return 'Showing delays for {}'.format(tube_line)
 
 app.run(debug=True)
